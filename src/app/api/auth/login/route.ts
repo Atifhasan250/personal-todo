f354730fd466046702c9e3de5b66dc631f55fdef
Atif Hasan <robiulhasan.atif@gmail.com>
2026-06-26 22:42:12 +0600

    fix: change cookie SameSite to lax to fix PWA home screen launch session drops

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -36,7 +36,7 @@ export async function POST(request: Request) {
       value: jwt,
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'lax',
       maxAge: 1296000,
       path: '/',
     });
